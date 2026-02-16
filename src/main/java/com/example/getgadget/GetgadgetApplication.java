3fafbf28906dbba5c9fab9d889b174a1491bfe87
Raimzhanov <Raimzhanov@lanit.ru>
2026-02-16 13:44:45 +0300

    Initial commit

---
+++ src/main/java/com/example/getgadget/GetgadgetApplication.java
@@ -0,0 +1,13 @@
+package com.example.getgadget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetgadgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetgadgetApplication.class, args);
+	}
+
+}
